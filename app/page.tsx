c7f526303188e1eacd31c655e3a72fea585f837b
Dandori-work <dw1003@Dandori-worknoMac-mini.local>
2025-09-21 08:49:58 +0900

    トップページ更新

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
     <main>
-      <h1>Hello World</h1>
+      <h1>こんにちは世界</h1>
+      <p>これは Vercel に公開された Next.js アプリです。</p>
     </main>
   );
 }
+
 
